0915382e463784bd036ce9086ad4ed68abfa9425
bereket-abera <bereket5042@gmail.com>
2020-12-28 08:16:25 +0000

    feat: first-working-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,226 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-elastic-sidebar';
+  title = 'angular-elastic-side-bar';
+  @ViewChild('path') path: ElementRef;
+  @ViewChild('sideBar') sideBar: ElementRef;
+  @ViewChild('sideBarContent') sideBarContent: ElementRef;
+  // @ViewChild('chat') chat: ElementRef;
+  @ViewChild('menu1') menu1: ElementRef;
+  sideBarTop;
+  sideBarLeft;
+
+  diffX = 0;
+  curX = 0;
+  finalX = 0;
+  frame = 1000 / 60;
+  animTime = 600;
+  sContTrans = 200;
+  animating = false;
+
+  startD;
+  midD;
+  finalD;
+  clickMidD;
+  clickMidDRev;
+  clickD;
+  currentPath;
+
+  easings = {
+    // returns new values for animation each time it is called
+    smallElastic: (t, b, c, d) => {
+      var ts = (t /= d) * t;
+      var tc = ts * t;
+      return (
+        b + c * (33 * tc * ts + -106 * ts * ts + 126 * tc + -67 * ts + 15 * t)
+      );
+    },
+    inCubic: (t, b, c, d) => {
+      var tc = (t /= d) * t * t;
+      return b + c * tc;
+    },
+  };
+
+  pathValue = 'M0,0 20,0 a0,250 0 1,1 0,500 L0,500';
+
+  ngOnInit() {}
+
+  /**
+   * creates a new arc parameters like a50,250
+   * by replacing the two centers with new ones
+   * @param {*} num1
+   * @param {*} num2
+   */
+  newD(num1, num2 = 250) {
+    let d = this.path.nativeElement.getAttribute('d');
+    num2 = num2;
+    let nd = d.replace(/\ba(\d+),(\d+)\b/gi, 'a' + num1 + ',' + num2);
+    return nd;
+  }
+
+  ngAfterViewInit() {
+    this.sideBarTop = this.sideBar.nativeElement.offsetTop;
+    this.sideBarLeft = this.sideBar.nativeElement.offsetLeft;
+    this.startD = this.createD(20, 0, 1);
+    this.midD = this.createD(125, 75, 0);
+    this.finalD = this.createD(200, 0, 1);
+    this.clickMidD = this.createD(300, 80, 0);
+    this.clickMidDRev = this.createD(200, 100, 1);
+    this.clickD = this.createD(300, 0, 1);
+    this.currentPath = this.startD;
+
+    this.handlers1();
+  }
+
+  createD(top, ax, dir) {
+    return 'M0,0 ' + top + ',0 a' + ax + ',250 0 1,' + dir + ' 0,500 L0,500';
+  }
+
+  animatePathD(
+    path,
+    d,
+    time,
+    handlers,
+    callback = null,
+    easingTop = null,
+    easingX = null
+  ) {
+    var steps = Math.floor(time / this.frame);
+    let curStep = 0;
+    let oldArr = this.currentPath.split(' ');
+    let newArr = d.split(' ');
+    let oldTop = +oldArr[1].split(',')[0];
+    let topDiff = +newArr[1].split(',')[0] - oldTop;
+    let nextTop;
+    let nextX;
+    easingTop = this.easings[easingTop] || this.easings.smallElastic;
+    easingX = this.easings[easingX] || easingTop;
+
+    const animate = () => {
+      curStep++;
+      nextTop = easingTop(curStep, oldTop, topDiff, steps);
+      nextX = easingX(curStep, this.curX, this.finalX - this.curX, steps);
+      oldArr[1] = nextTop + ',0';
+      oldArr[2] = 'a' + Math.abs(nextX) + ',250';
+      oldArr[4] = nextX >= 0 ? '1,1' : '1,0';
+      this.pathValue = oldArr.join(' ');
+      if (curStep > steps) {
+        this.curX = 0;
+        this.diffX = 0;
+        this.pathValue = d;
+        this.currentPath = d;
+        if (handlers) this.handlers1();
+        if (callback) callback();
+        return;
+      }
+      requestAnimationFrame(animate);
+    };
+    animate();
+  }
+
+  handlers1() {
+    const mousedownTouchstart = (event) => {
+      var startX = event.pageX || event.targetTouches[0].pageX;
+
+      // update path when the user drags the white side bar ( creates curve like structure )
+      const touchmoveMousemove = (e) => {
+        var x = e.pageX || e.changedTouches[0].pageX;
+        this.diffX = x - startX;
+        // console.log(x, startX, this.diffX);
+        if (this.currentPath == this.finalD) return;
+        if (this.diffX < 0) this.diffX = 0;
+        if (this.diffX > 300) this.diffX = 300;
+        this.curX = Math.floor(this.diffX / 2);
+        this.pathValue = this.newD(this.curX);
+      };
+      document.ontouchmove = touchmoveMousemove;
+      document.onmousemove = touchmoveMousemove;
+    };
+
+    this.sideBar.nativeElement.onmousedown = mousedownTouchstart;
+    this.sideBar.nativeElement.ontouchstart = mousedownTouchstart;
+
+    const mouseupTouchEnd = () => {
+      const touchmoveMousemove = (e) => {
+        e.preventDefault();
+      };
+      document.onmousemove = touchmoveMousemove;
+      document.ontouchmove = touchmoveMousemove;
+
+      if (this.animating) return;
+      if (!this.diffX) return;
+      if (this.currentPath == this.finalD) return;
+      if (this.diffX < 40) {
+        this.animatePathD(null, this.newD(0), this.animTime, true);
+      } else {
+        this.animatePathD(null, this.finalD, this.animTime, false, () => {
+          // this.chat.nativeElement.style.display = 'block';
+          this.closeSidebar();
+          setTimeout(() => {
+            document.onclick = (e) => {
+              this.closeSidebar(e);
+            };
+          }, this.sContTrans);
+        });
+      }
+    };
+
+    document.onmouseup = mouseupTouchEnd;
+    document.ontouchend = mouseupTouchEnd;
+  }
+
+  closeSidebar(e = null, menu = null) {
+    if (e && e.clientX < 200 && !menu) return;
+    if (this.animating) return;
+    this.sideBarContent.nativeElement.classList.add('active');
+    if (!e) return;
+    this.animating = true;
+    this.sideBarContent.nativeElement.classList.remove('active');
+    this.finalX = -75;
+    setTimeout(() => {
+      this.animatePathD(
+        null,
+        this.midD,
+        this.animTime / 3,
+        false,
+        () => {
+          // this.chat.nativeElement.style.display = 'none';
+          this.finalX = 0;
+          this.curX = -75;
+          this.animatePathD(null, this.startD, (this.animTime / 3) * 2, true);
+          this.animating = false;
+        },
+        'inCubic'
+      );
+    }, this.sContTrans);
+    document.onclick = (event) => {
+      event.preventDefault();
+    };
+  }
+
+  collapse(event) {
+    console.log(event);
+    setTimeout(() => {
+      this.closeSidebar(event, 'menu');
+    }, 400);
+  }
+
+  // ripple(elem, e) {
+  //   var elTop = elem.offset().top,
+  //     elLeft = elem.offset().left,
+  //     x = e.pageX - elLeft,
+  //     y = e.pageY - elTop;
+  //   var $ripple = $("<div class='ripple'></div>");
+  //   $ripple.css({ top: y, left: x });
+  //   elem.append($ripple);
+  // }
+
+  menuClick(event) {
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,85 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzResultModule } from 'ng-zorro-antd/result';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzButtonModule,
+    NzGridModule,
+    NzDividerModule,
+    NzSelectModule,
+    NzIconModule,
+    NzTypographyModule,
+    NzTableModule,
+    NzInputNumberModule,
+    NzModalModule,
+    NzNotificationModule,
+    NzPaginationModule,
+    NzResultModule,
+    NzInputModule,
+    NzFormModule,
+    NzCardModule,
+    NzListModule,
+    NzBadgeModule,
+    NzAvatarModule,
+    NzDropDownModule,
+    NzDropDownModule,
+    NzDatePickerModule,
+    NzPopoverModule,
+    NzEmptyModule,
+    NzLayoutModule,
+    NzSpaceModule,
+    NzSpinModule,
+    NzMessageModule,
+    NzTagModule,
+    NzCollapseModule,
+    NzProgressModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
